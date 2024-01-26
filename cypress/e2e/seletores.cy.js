bc6d1c6b20d4930be2bf39995dad76fe70654ae1
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2024-01-26 00:14:54 -0300

    aula seletores cy

--- cypress/e2e/seletores.cy.js
+++ cypress/e2e/seletores.cy.js
@@ -7,53 +7,51 @@ describe('Seletores avançados com cypress', () => {
   });
 
   it('Seleciona elementos que contêm um Texto específico', () => {
-    cy.contains('Item 3').should('have.attr', 'class', 'filho-3')
+    //TODO: 
   });
     
   it('Seleciona o elemento com a classe pai', () => {
-   cy.get('.pai').should('exist');
+    //TODO: 
   })
 
   it('Seleciona o elemento com o id Filho', () => {
-    cy.get('#id-filho').should('exist');
+    //TODO: 
    })
 
   it('Seleciona um elemento filho dentro do elemento com a classe pai', () => {
-    cy.get('.pai').find('.filho-2').should('contain', 'Item 2')
+    //TODO: 
   });
 
   it('Seleciona o segundo elemento <span> com a classe irmao', () => {
-    cy.get('.irmao + .irmao').should('contain', 'Irmão 2')
+    //TODO: 
   });
 
   it('Seleciona o próximo elemento irmão', () => {
-    cy.get('.irmao').next().should('contain', 'Irmão 2')
+    //TODO: 
   });
 
   it('Seleciona o elemento irmão anterior', () => {
-    cy.get('#irmao-2').prev().should('contain.text','Irmão 1' );
+    //TODO: 
   });
 
   it('Seleciona o irmão da div anterior', () => {
-    cy.get('.pai-tio-1').prev().should('contain.text','Item 1' );
+    //TODO: 
   });
 
   it('Seleciona o terceiro elemento <li> encontrado', () => {
-    cy.get('li').eq(2).should('have.text', 'Item 3')
+    //TODO: 
   });
 
   it('Seleciona o elemento com o atributo data-test', () => {
-    cy.get('[data-test="div-pai"]').should('exist')
+    //TODO: 
   });
 
   it('Seleciona o elemento com a classe pai do elemento com a classe filho', () => {
-    cy.get('.filho-4').parents('.pai').should('have.attr', 'class', 'pai')
+    //TODO: 
   });
 
   it('Seleciona o elemento com um valor em um select', () => {
-    cy.get('[name="opcao"]').select('Pouco')
-    cy.get('#id-enviar').click()
-    cy.get('#mensagemFeedback').should('have.text', 'Obrigado pelo seu feedback')
+    //TODO: 
   });
 
 })
